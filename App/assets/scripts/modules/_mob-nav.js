c7f0b4209b6a1c998d9f736aafb4a4619b3810d5
unknown <AyoubYaacoubi16@gmail.com>
2021-03-01 05:59:37 +0100

    transitionend event

--- App/assets/scripts/modules/_mob-nav.js
+++ App/assets/scripts/modules/_mob-nav.js
@@ -2,7 +2,25 @@
 export default class MobNav{
     constructor(){
         // variables and initializing the events:
-        
+        this.burgerMenu = document.querySelector('.burger-menu')
+        this.mobNavBar = document.querySelector('.header__mob-nav')
 
+        // initializing the events:
+        this.event()
+    }
+    // events:
+    event(){
+        this.burgerMenu.addEventListener('click', () => this.animateBurger())
+        this.burgerMenu.addEventListener('transitionend', ()=> this.showMobNav())
+    }
+    // all the methods here:
+
+    // showing the nav bar en activating the burgers:
+    animateBurger(){
+        this.burgerMenu.classList.toggle('burger-menu--active')
+    }
+
+    showMobNav(){
+        console.log('hellow')
     }
 }
