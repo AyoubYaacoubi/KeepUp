2a9ca34d811b08f6dbe39972416c42743823bde9
unknown <AyoubYaacoubi16@gmail.com>
2021-03-02 05:59:05 +0100

    mobile nav

--- App/assets/scripts/modules/_mob-nav.js
+++ App/assets/scripts/modules/_mob-nav.js
@@ -3,7 +3,7 @@ export default class MobNav{
     constructor(){
         // variables and initializing the events:
         this.burgerMenu = document.querySelector('.burger-menu')
-        this.mobNavBar = document.querySelector('.header__mob-nav')
+
 
         // initializing the events:
         this.event()
@@ -11,16 +11,11 @@ export default class MobNav{
     // events:
     event(){
         this.burgerMenu.addEventListener('click', () => this.animateBurger())
-        this.burgerMenu.addEventListener('transitionend', ()=> this.showMobNav())
     }
     // all the methods here:
 
     // showing the nav bar en activating the burgers:
     animateBurger(){
         this.burgerMenu.classList.toggle('burger-menu--active')
-    }
-
-    showMobNav(){
-        console.log('hellow')
     }
 }
