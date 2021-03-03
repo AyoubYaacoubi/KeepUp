8c4da94d33ddde051edc7ba5c4dc6638fab975ef
unknown <AyoubYaacoubi16@gmail.com>
2021-03-03 05:22:13 +0100

    burger menu ends

--- App/assets/scripts/modules/_mob-nav.js
+++ App/assets/scripts/modules/_mob-nav.js
@@ -3,8 +3,8 @@ export default class MobNav{
     constructor(){
         // variables and initializing the events:
         this.burgerMenu = document.querySelector('.burger-menu')
-
-
+        this.navBar = document.querySelector('.header__navbar')
+        
         // initializing the events:
         this.event()
     }
@@ -17,5 +17,6 @@ export default class MobNav{
     // showing the nav bar en activating the burgers:
     animateBurger(){
         this.burgerMenu.classList.toggle('burger-menu--active')
+        this.navBar.classList.toggle('header__navbar--active')
     }
 }
